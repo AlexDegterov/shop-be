d305d44b8ffcabeacdea4a256f487a6a019baf5d
alexd-brisance <alexd@brisance.digital>
2021-09-10 12:53:05 +0300

    Added work with RDS

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,14 +1,10 @@
 import * as handlers from './src/handlers';
 import { Client } from 'pg';
-// import { PostgresProductService } from './src/services/postgres-memory-product-service';
-import { InMemoryProductService } from './src/services/in-memory-product-service';
-
-console.log(process.env);
+import { PostgresProductService } from './src/services/postgres-memory-product-service';
 
 const databaseClient = new Client();
 databaseClient.connect();
-const productService = new InMemoryProductService();
-// const productService = new PostgresProductService(databaseClient)
+const productService = new PostgresProductService(databaseClient);
 
 export const getProductById = handlers.getProductByIdHandler(productService);
 export const getProductsList = handlers.getProductsListHandler(productService);
