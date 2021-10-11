2b3ee67eedec3b3ec3f9166b3c9bc2d97808b2b7
AlexDegterov <degterov@proektant.by>
2021-10-11 23:52:35 +0300

    Fixed adding info into DB

--- src/product-service/src/services/postgres-memory-product-service.ts
+++ src/product-service/src/services/postgres-memory-product-service.ts
@@ -4,6 +4,7 @@ import { Client, QueryConfig } from 'pg';
 class PostgresProductService implements ProductServiceInterface {
 
     private tableName = 'products';
+    private tableNameStocks = 'stocks';
 
     constructor(private databaseClient: Client){}
 
@@ -27,13 +28,20 @@ class PostgresProductService implements ProductServiceInterface {
         return result.rows ? result.rows : null;
     }
 
-    async create(product: Pick<ProductInterface, 'title' | 'description' | 'price' | 'logo'>) {
-        let { title, description, price, logo } = product;
+    async create(product: Pick<ProductInterface, 'title' | 'description' | 'price' | 'logo' | 'count'>) {
+        let { title, description, price, logo, count } = product;
         const query = {
             text: `INSERT INTO ${this.tableName}(title, description, price, logo) VALUES($1, $2, $3, $4) RETURNING *`,
             values: [title, description, price, logo],
         };
         const result = await this.databaseClient.query(query);
+        if (result.rows[0]){
+            const query2 = {
+                text: `INSERT INTO ${this.tableNameStocks}(product_id, count) VALUES($1, $2) RETURNING *`,
+                values: [result.rows[0]['id'], count],
+            };
+            await this.databaseClient.query(query2);
+        }
         return result.rows[0] ? result.rows[0] : null;
     }
 }
