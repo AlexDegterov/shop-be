dfdd1b2608f006025230d80177bdc7ccd93c8eee
AlexDegterov <degterov@proektant.by>
2021-10-14 07:06:49 +0300

    Removed aws-sdk and fixed Action

--- src/authorization-service/src/basicAuthorizer.ts
+++ src/authorization-service/src/basicAuthorizer.ts
@@ -2,11 +2,12 @@
 export const basicAuthorizerHandler = () => async (event, ctx, cb) => {
     try {
         console.log('Event', JSON.stringify(event));
-
+        console.log('process.env.NAME', process.env.NAME);
+        console.log('process.env.PASSWORD', process.env.PASSWORD);
         if (event['type'] != 'TOKEN') {
             cb('Unauthorized');
         }
-
+        
         const NAME = process.env.NAME;
         const PASSWORD = process.env.PASSWORD;
         const authorizationToken = event.authorizationToken;
@@ -18,8 +19,11 @@ export const basicAuthorizerHandler = () => async (event, ctx, cb) => {
 
         console.log(`username: ${username} and password: ${password}`);
 
-        const effect = NAME != username || PASSWORD != password ? 'Deny' : 'Allow';
-        const policy = generatorPolicy(encodedCreds, event.methodArm, effect);
+        const effect = (NAME == username && PASSWORD == password) ? 'Allow' : 'Deny';
+        console.log('effect', effect);
+
+        const policy = generatorPolicy(encodedCreds, event.methodArn, effect);
+        console.log(JSON.stringify(policy));
         cb(null, policy);
 
     } catch (error) {
@@ -35,7 +39,7 @@ const generatorPolicy = (principalId, resource, effect = 'Allow') => {
             Version: '2012-10-17',
             Statement: [
                 {
-                    Action: 'execute-api:Invoke',
+                    Action: "execute-api:Invoke",
                     Effect: effect,
                     Resource: resource
                 }
